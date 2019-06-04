a7d6ea35c30f3b11831321ed468bcd0c057ac63d
sftd114 <sftd.an@gmail.com>
2019-06-04 10:05:19 +0300

    second test commit

--- FreqDict/TestClass.cs
+++ FreqDict/TestClass.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace FreqDict
 {
+    [TestFixture]
     class TestClass
     {
+        [Test]
+        public void PathTest()
+        {
+            FreqClass freq = new FreqClass();
+            freq
+        }
     }
 }
